5391d3422cc3b04cf9de8e91b31fbad4ed101f9a
Andraz Skupek <andraz.skupek@nxp.com>
2023-01-09 09:35:53 +0100

    Delete patches

--- install_NFC.sh
+++ install_NFC.sh
@@ -20,8 +20,8 @@ cd $ANDROID_BUILD_TOP/build/make
 patch -p1 <$ANDROID_BUILD_TOP/vendor/nxp/nfc/patches/AROOT_build_make.patch
 cd $ANDROID_BUILD_TOP/hardware/interfaces
 patch -p1 <$ANDROID_BUILD_TOP/vendor/nxp/nfc/patches/AROOT_hardware_interfaces.patch
-cd $ANDROID_BUILD_TOP/system/core
-patch -p1 <$ANDROID_BUILD_TOP/vendor/nxp/nfc/patches/AROOT_system_core.patch
+cd $ANDROID_BUILD_TOP/system/nfc
+patch -p1 <$ANDROID_BUILD_TOP/vendor/nxp/nfc/patches/AROOT_system_nfc.patch
 cd $ANDROID_BUILD_TOP/system/logging
 patch -p1 <$ANDROID_BUILD_TOP/vendor/nxp/nfc/patches/AROOT_system_logging.patch
 cd $ANDROID_BUILD_TOP
